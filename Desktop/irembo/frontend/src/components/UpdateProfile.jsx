d58aa65240d4914405089a90c2c4b1866230c903
Robertisimwe <robertishimwe0@gmail.com>
2023-09-14 14:35:28 +0200

    added validation

--- Desktop/irembo/frontend/src/components/UpdateProfile.jsx
+++ Desktop/irembo/frontend/src/components/UpdateProfile.jsx
@@ -242,7 +242,8 @@ export default function UpdateProfile() {
                 </Grid>
               </Grid>
               {/** add togle button to unable 2fa or disable it **/}
-              <Typography component="h1" variant="h5">
+              <br/>
+              <Typography component="h3" variant="h5">
               Enable Two Factor Authentication
             </Typography>
               <Grid item xs={12}>
